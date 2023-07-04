f255fdbbb6e5d5c3c049b80b59794d53bccc4de2
zxyao145 <zxyao145@gmail.com>
2023-07-05 00:11:35 +0800

    feat: PageIndicator

---
+++ docs/BlazorMix.Docs/Demos/Components/PageIndicator/Docs/index.zh-CN.md
@@ -0,0 +1,28 @@
+# PageIndicator 分页符
+
+指示当前显示的是多页面视图的哪一页。
+
+## 何时使用
+
+适用于表示当前内容区块可以横向滚动翻页。
+
+<code-demo Src="Demos/Components/PageIndicator/Demos/Demo1"></code-demo>
+
+## API
+
+### 属性
+
+> [xmldoc]
+
+### CSS 变量
+
+| 属性 | 说明 | 默认值 |
+| --- | --- | --- |
+| --active-dot-border-radius | 当前页的圆角大小 | `var(--dot-border-radius)` |
+| --active-dot-color | 当前页的颜色 | `var(--adm-color-primary)` |
+| --active-dot-size | 当前页的大小 | `13px` |
+| --dot-border-radius | 非当前页的圆角大小 | `1px` |
+| --dot-color | 非当前页的颜色 | `#dddddd` |
+| --dot-size | 非当前页的大小 | `3px` |
+| --dot-spacing | 页码指示器小圆点之间的间距 | `3px` |
+
---
+++ src/BlazorMix/PageIndicator/PageIndicator.razor.cs
@@ -0,0 +1,46 @@
+﻿
+namespace BlazorMix;
+public partial class PageIndicator
+{
+    public const string ClsPrefix = "bm-page-indicator";
+    private const string ClsDotActive = $"{ClsPrefix}-dot-active";
+
+
+    /// <summary>
+    /// 总页数	
+    /// </summary>
+    [Parameter]
+    public int Total { get; set; }
+
+    /// <summary>
+    /// 当前页（从 0 开始计数）	
+    /// </summary>
+    [Parameter]
+    public int Current { get; set; }
+
+    /// <summary>
+    /// 方向，默认是水平方向	
+    /// </summary>
+    [Parameter]
+    public BmDirection Direction { get; set; }
+
+    /// <summary>
+    /// 颜色（'primary' | 'white'	）
+    /// </summary>
+    [Parameter]
+    public string Color { get; set; } = "primary";
+
+
+    protected override void OnInitialized()
+    {
+        _classBuilder.Clear()
+            .Add(ClsPrefix)
+            .Add($"{ClsPrefix}-{Direction.GetDisplayName()}")
+            .Add($"{ClsPrefix}-color-{Color}")
+            .Add(() => Class?.ToString() ?? "")
+            ;
+        _styleBuilder.Clear()
+            .Add(() => Style?.ToString() ?? "")
+            ;
+    }
+}
